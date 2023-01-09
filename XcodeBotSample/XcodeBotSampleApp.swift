cfe94d33592ab79452b14e2a81396ce50390e158
Qasim Abbas <qasimabbas52@gmail.com>
2023-01-09 04:24:21 -0500

    Inital Commit

---
+++ XcodeBotSample/XcodeBotSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeBotSampleApp.swift
+//  XcodeBotSample
+//
+//  Created by Qasim Abbas on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct XcodeBotSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XcodeBotSampleTests/XcodeBotSampleTests.swift
@@ -0,0 +1,18 @@
+//
+//  XcodeBotSampleTests.swift
+//  XcodeBotSampleTests
+//
+//  Created by Qasim Abbas on 1/9/23.
+//
+
+import XCTest
+@testable import XcodeBotSample
+
+final class XcodeBotSampleTests: XCTestCase {
+
+    func testSuccessfulAssert() {
+        XCTAssertTrue(true)
+    }
+
+}
+ 
